d730c8ef9a177d5554ca9d89d5f8ed27f48612bb
Jayachandran <jayachandran.sa@outlook.com>
2025-01-18 17:07:18 +0530

    Added a newline in dummy branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Dummy
 #This lines were updated in dummy branch and going to merge with main brach soon
 def quicksort():
+    def quicksort_dummybranch():
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr)//2]
